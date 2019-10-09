48d165c6196b0b0124d2259072ea0bada60a15ca
Sachin Daingade <sbdaingade@gmail.com>
2019-10-09 21:13:29 +0530

    Initial commit

---
+++ iosAutomation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosAutomation
+//
+//  Created by Sachin Daingade on 09/10/19.
+//  Copyright © 2019 Sachin Daingade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
